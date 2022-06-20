d4116a2541efeb722227ba3a9b923488c216c7be
Opie Taylor <taborn.nequil@gmail.com>
2022-06-20 15:57:15 -0400

    nequil group reg logic

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,10 +1,61 @@
 import { Box, TextField } from "@mui/material";
+import { useState, SyntheticEvent } from "react";
+import { User } from "../models/user";
 
 interface IRegisterProps {
 
-}
+        // fields any methods : interface is most appropriate
+    
+        currentUser: User | undefined | null,  // union type
+    
+        // update function
+        setCurrentUser: (nextUser: User) => void
+    }
+
 
 function Register(props: IRegisterProps) {
+
+    const [username, setUsername] = useState(''); // let allows reassignment
+    const [password, setPassword] = useState(''); // const restricts that ^
+    const [fname, setFirstname] = useState('');
+    const [lname, setLastname] = useState('');
+    const [errorMsg, setErrorMsg] = useState<string>(); 
+    
+    let login = async (e: SyntheticEvent) => {
+            
+        e.preventDefault(); // prevent default event logic from running
+        // like if you use form, prevent it from sending a GET request with params
+        // because we want to control what the button does.
+
+        if (!username || !password || !fname || !lname) {
+            setErrorMsg("You must provide a username, password, firstname, lastname");
+        }
+        else {
+            setErrorMsg('');
+            try {
+                let resp = await fetch('http://localhost:5000/notecard/auth', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({username, password, fname, lname})
+                });
+                
+                if (Math.floor(resp.status/100) !== 2) {
+                    setErrorMsg("Could not validate provided credentials!")
+                    let data = await resp.text();
+                    setErrorMsg(data);
+
+                } else {
+                    let data = await resp.json();
+                    props.setCurrentUser(data);
+                }   
+            } catch(err) {
+                setErrorMsg("There was an error communicating with the API");
+            } 
+        }
+    }
+    
     return ( 
     <>
         <h4>Please enter registration details</h4>
